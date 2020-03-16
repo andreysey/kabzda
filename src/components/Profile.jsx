e2690186f17429031e62686f0fee6b789c285747
andreysey <andreyseynew@gmail.com>
2020-03-16 19:36:05 +0200

    add my first project and all files

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png' alt='wide content'/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Profile;
